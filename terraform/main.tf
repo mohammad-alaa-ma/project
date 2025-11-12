a3933d4f11a58749b598e8b52170b681fec9e804
Mohammad Alaa <mohammad.alaaeldin98@gmail.com>
2025-11-12 16:28:50 +0200

    Add AWS instance resource for Jenkins

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,14 @@ resource "aws_instance" "test_instance" {
   }
 }
 
+resource "aws_instance" "test_instance" {
+  ami           = "ami-0ecb62995f68bb549" 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Jenkins-Terraform-Test 2"
+  }
+}
 
 
 
